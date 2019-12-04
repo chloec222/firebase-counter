dab9984b78327f0bad98fd68017f3b83a5c60fa0
russomp <russomp@usc.edu>
2019-12-03 21:38:37 -0500

    Manage count updates using firestore updates and snapshot events

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -1,12 +1,32 @@
-// util function that wraps querySelector
-const qs = selector => document.querySelector(selector);
-// references to our count elements
-const countSpan = qs(".counter__count");
-const counterBtn = qs(".counter__btn");
-// initialize state
-let count = 0;
-// update UI and state on button click
+// Set up helpers
+var qs = selector => document.querySelector(selector);
+var counterBtn = qs(".counter__btn");
+var countSpan = qs(".counter__count");
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDvZX-OHNy_FEGpZsscJ-Svg2shqwQPPQQ",
+  authDomain: "fsf-counter-demo.firebaseapp.com",
+  databaseURL: "https://fsf-counter-demo.firebaseio.com",
+  projectId: "fsf-counter-demo",
+  storageBucket: "fsf-counter-demo.appspot.com",
+  messagingSenderId: "604340790600",
+  appId: "1:604340790600:web:987d1cad731346c2d2bf7d"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize db and count refs
+var db = firebase.firestore();
+var counterRef = db.collection("counter").doc("Gfo0bReF1XorHTSQ79Kt");
+
 counterBtn.addEventListener("click", () => {
-  count++;
-  countSpan.textContent = count;
+  counterRef
+    .update({ count: firebase.firestore.FieldValue.increment(1) })
+    .then(() => console.log("[DEBUG] successfully updated count"));
+});
+
+counterRef.onSnapshot(doc => {
+  console.log(`count updated to ${JSON.stringify(doc.data())}`);
+  countSpan.textContent = doc.data().count;
 });
